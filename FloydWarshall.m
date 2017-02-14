35083062276fb9ff01445c125c02beb8b6bc1c04
Stamatis Pitsios <stamatis.pit@gmail.com>
2017-02-14 16:54:47 +0200

    Update FloydWarshall.m

--- FloydWarshall.m
+++ FloydWarshall.m
@@ -1,9 +1,9 @@
-function [D,P] = FloydWarshall(W)
+function [D, P] = FloydWarshall(W)
 
 % This function computes shortest paths' distances for each pair of nodes
 % within the graph whose initial weight (adjacency) matrix is stored in
 % matrix W. Matrix W is assummed to be properly initialized.
-% Element D[i,j] of matrix D stores the shortest path distance from node i 
+% Element D[i,j] of matrix D stores the shortest path distance from node i
 % to node j.
 % Matrix P is the corresponding predecessor matrix so that element P[i,j]
 % stores the last vertex traversed within the shortest path connecting
@@ -16,15 +16,22 @@ nodes_num = size(W,1);
 D = W;
 
 % Initialize internal matrix P.
-P = zeros(nodes_num,nodes_num);
+P = cell(nodes_num);
+for i = 1:1:nodes_num
+    for j = 1:1:nodes_num
+        P{i, j} = [-1];
+    end;
+end;
 
 % Main Algorithm.
 for k = 1:1:nodes_num
     for i = 1:1:nodes_num
         for j = 1:1:nodes_num
-            if(D(i,j) > D(i,k) + D(k,j))
-                D(i,j) = D(i,k) + D(k,j);
-                P(i,j) = k;
+            if (D(i, j) > D(i, k) + D(k, j))
+                D(i, j) = D(i, k) + D(k, j);
+                P{i, j} = [k];
+            elseif (D(i, j) == D(i, k) + D(k, j) && i ~= k && j ~= k)
+                P{i, j} = [P{i, j}, k];
             end;
         end;
     end;
