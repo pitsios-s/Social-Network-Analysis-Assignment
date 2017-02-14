39debeadd8c6976a0caf5e51e18039a82b1317fb
Stamatis Pitsios <stamatis.pit@gmail.com>
2017-02-14 16:54:27 +0200

    Update Path.m

--- Path.m
+++ Path.m
@@ -1,25 +1,25 @@
-function [SP] = Path(u,v,P,D,SP)
+function [SP] = Path(u, v, P)
 
 % This function returns the vector SP which corresponds to the sequence of
-% graph nodes composing the shortest path from node u to node v given the 
+% graph nodes composing the shortest path from node u to node v given the
 % predecessor matrix P and the final shortest paths distance matrix D.
 % Note that vector SP must be initially passed as an empty matrix.
+%
+%  Source: http://stackoverflow.com/questions/11370041/floyd-warshall-all-shortest-paths
 
-% Get intermediate path vertices.
-if(P(u,v)~=0)
-    Path(u,P(u,v),P,D,SP);
-    SP = [SP,P(u,v)];
-    Path(P(u,v),v,P,D,SP);
-end;
-
-% Compose full path.
-if(P(u,v)~=0)
-    SP = [u,SP,v];
-else
-    if(and((D(u,v)~=0),(D(u,v)~=Inf)))
-        SP = [u,v];
+SP = {};
+if (~isempty(P{u, v}))
+    for (k = P{u, v})
+        if (k == -1)
+            SP{end + 1} = [u v];
+        else
+            paths_I_K = Path(u, k, P);
+            paths_K_J = Path(k, v, P);
+            for i_k = paths_I_K
+                for k_j = paths_K_J
+                    SP{end + 1} = [i_k{1}  k_j{1}(2)];
+                end;
+            end;
+        end;
     end;
 end;
-
-end
-
